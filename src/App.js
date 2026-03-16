e1d2dd29cf0acf53717718c66bf5d1a042a64365
Aryan-basatia <aryanbasatia24@gmail.com>
2026-03-16 21:18:15 +0530

    Update title and implement initial items in PackingList component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
+
 export default function App() {
   return (
-  <div className="app">
-    <Logo />
-    <Form />
-    <PackingList />
-    <Stats />
-  </div>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
   );
 }
 
@@ -22,12 +28,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>👜 You have X items on your list, and you already packed X (X%) </em>
     </footer>
   );
